81f385afe11b721be2a64ba0cbda89b0e5195b65
Camilo Bustos <bu_ivan@javeriana.edu.co>
2023-08-09 22:35:06 -0500

    ya casi lo pinto

--- main.py
+++ main.py
@@ -30,10 +30,23 @@ for cd in cuadrados:
 
 
 #pintamos matriz con cuadrados 
-for x in range(maxX-1):
+"""for x in range(maxX-1):
     for y in range(maxY-1):
-        
         print('* ', end='')
+    print()"""
+
+#pintar matrizcon sus respectivos cuadros 
+for cd in lista_cuadrados:
+    for x in range(maxY + 1):
+        for y in range(maxX + 1):
+            if cd[1]  == x and cd[0] <= y and cd[3] >= y :
+                    print('o ', end='')
+            elif cd[0] == y and cd[2] <= x :
+                print('o ', end='')
+
+            else:
+                print('- ', end='')
+        print()
+    print()
     print()
 
-#pintar matriz
