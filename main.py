da7a9b573965db9ae9674faf71a0fed80f389833
Camilo Bustos <bu_ivan@javeriana.edu.co>
2023-08-11 12:47:24 -0500

    ya se pinto todo el cuadro con todos los cudadros internos

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ ruta = os.path.join(os.path.dirname(__file__),'matriz_ejemplo.txt')
 maxX = 0
 maxY = 0
 lista_cuadrados  = [[]]
+
 with open(ruta,'r') as matriz:
     cuadrados = matriz.readlines()
 
@@ -36,24 +37,44 @@ for cd in cuadrados:
     print()"""
 
 #pintar matrizcon sus respectivos cuadros 
+
+matriz_final = [['@' for _ in range(maxY + 1)] for _ in range(maxX + 1)]
 for cd in lista_cuadrados:
     for x in range(maxY + 1):
         for y in range(maxX + 1):
-            valor0 = abs(cd[0]- 6)
             valor3 = abs(cd[3]- 6)
             valor1 = abs(cd[1]- 6)
-            valor2 = abs(cd[2]- 6)
             if valor1  == x   and cd[2] >= y and cd[0] <= y:
-                    print('o ', end='')
+                matriz_final[x][y] = '+' 
+                print('* ', end='')
             elif valor3 == x and cd[2] >= y and cd[0] <= y:
-                print('o ', end='')
+                print('* ', end='')
+                matriz_final[x][y] = '+' 
             elif cd[0] == y and valor3 >= x and valor1 <= x:
-                print('o ', end='')
+                print('* ', end='')
+                matriz_final[x][y] = '+'  
             elif cd[2] == y and valor3 >= x and valor1 <= x:
-                print('o ', end='')
+                print('* ', end='')
+                matriz_final[x][y] = '+' 
             else:
-                print('- ', end='')
+                print('* ', end='')
+
         print()
+
     print()
     print()
 
+
+#pintar matriz con los cuados pintados 
+for linea in matriz_final:
+    print(linea)
+
+#función para sacar el area total de los cuadrados de manera fuerza bruta 
+def area_fuerza_bruta(matriz):
+    pass
+
+
+#función para sacar el area de forma recursiba 
+
+def area_recursiba(matriz):
+    pass
